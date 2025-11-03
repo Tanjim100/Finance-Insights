7fb1029a4dcac3f2c35475d69943861ec0a5b402
Tanjim100 <tanjim.haider.12112100@gmail.com>
2025-11-04 04:16:20 +0600

    blog card alignmet fixed

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -20,7 +20,7 @@ const BlogCard = ({ blog }) => {
 
     return (
         <div>
-            <article className="overflow-hidden rounded-xl border border-gray-100 bg-white shadow-xs">
+            <article className="overflow-hidden rounded-xl border border-gray-100 bg-white shadow-xs ">
                 <img
                     alt=""
                     src={header_image_url}
@@ -28,21 +28,23 @@ const BlogCard = ({ blog }) => {
                 />
 
                 <div className="p-4 sm:p-6">
-                    <h6 className='text-sm mb-3 text-gray-600'>{categories.name}</h6>
-                    {/* <h6 className='text-sm mb-3 text-gray-600'></h6> */}
-                    <a href="#">
-                        <h3 className="text-base md:text-lg font-medium text-gray-900">
-                            {title}
-                        </h3>
-                    </a>
+                    <div className='h-40'>
+                        <h6 className='text-sm mb-3 text-gray-600'>{categories.name}</h6>
+                        {/* <h6 className='text-sm mb-3 text-gray-600'></h6> */}
+                        <a href="#">
+                            <h3 className="text-base md:text-lg font-medium text-gray-900 line-clamp-2">
+                                {title}
+                            </h3>
+                        </a>
 
-                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 line-clamp-3">
-                        {description}
-                    </p>
+                        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                            {description}
+                        </p>
+                    </div>
 
-                    <div className="group mt-4 flex items-center gap-1 text-sm font-medium justify-between text-[#01101C]">
+                    <div className="group mt-4 flex items-center gap-1 text-sm justify-between text-[#01101C]">
                         <p>{formatted_published_date}</p>
-                        <a href="blog_id" className="inline-flex items-center gap-1 ">
+                        <a href="blog_id" className="inline-flex items-center gap-1 font-medium">
                             Read more
 
                             <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
